f993ed6b48fd62c27fb3fa0d790455af78fb506e
Kim Min Sung <1nnovator1105@gmail.com>
2025-07-07 20:08:17 +0900

    build: 빌드 에러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-
 export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
